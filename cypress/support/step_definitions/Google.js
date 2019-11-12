baf37234b369842d62bf83a17877c9a6af7c9dfc
Galib Hassan <Galib.Hassan@nimbix.net>
2019-11-12 11:34:11 -0600

    google

--- cypress/support/step_definitions/Google.js
+++ cypress/support/step_definitions/Google.js
@@ -1,10 +1,10 @@
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
 
-cy.log('cucumber found me')
+cy.log('cucumber found me');
 
 const url = 'https://google.com'
 Given('I open Google page', () => {
-  cy.log('going to visit', url)
+  cy.log('going to visit', url);
 
   cy.visit(url)
 })
